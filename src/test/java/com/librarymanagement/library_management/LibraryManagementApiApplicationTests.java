c6a33a844937722790a422a641c7668795030a9e
unknown <mostafa.moharm2001@gmail.com>
2024-08-10 07:37:09 +0300

    Initial Project Setup

---
+++ src/test/java/com/librarymanagement/library_management/LibraryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
